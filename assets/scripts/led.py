e3475adaf406d29ec6b21a64f70848d89583ea7f
Magnus <{25343537}+{magborresen}@users.noreply.github.com>
2018-11-14 12:27:07 +0100

    led functionality

--- assets/scripts/led.py
+++ assets/scripts/led.py
@@ -1,4 +1,19 @@
 from giozero import LED
 from gpiozero.pins.pidpio import PigPIOFactory
+import json
 
-factory = PigPIOFactory(host=ip)
+file_loc = "../../user-info.json"
+
+
+def get_data(json_file):
+    data = json.loads(json_file)
+    ip = data['IP-Address']
+
+    return ip
+
+
+def led_turn_on(ip):
+
+    factory = PigPIOFactory(host=ip)
+    led = LED(17, pin_factory=factory)
+    
